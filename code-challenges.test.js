9bdac07c1e1dd65786d2529d8f71e244f28aeb9a
tabithatrubey <tabithatrubey@outlook.com>
2022-11-13 12:40:12 -0800

    Week3 Assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,54 +14,138 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("greaterNum", () => {
+  it ("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+    const fibLength1 = 6
+    // Expected output: [1, 1, 2, 3, 5, 8]
+    const fibLength2 = 10
+    // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(greaterNum(6)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(greaterNum(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  }
+  )
+}
+)
+// ReferenceError: greaterNum is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// Pseudocode:
+// Create a function called greaterNum
+// The function has one parameter 'n' that indicates the number that will be passed into the function
+// Create a variable fib to identify the numbers we already know in the sequence, 1 and 1.
+// Create a conditional statement to determine if:
+  // Identify of the input is set at the value of greater than 2
+    // if the number is greater than 2 create a for loop to iterate through the array of the given number until we have the index of the given number.
+      // We know that the index will start at the index of 2 since the first two numbers are already given to us and their indexes are 0 and 1 respectively. 
+    // For each iteration, we want to populate the index of i so fib at the index of i is set to the previous two numbers added together.
+      // Return this value of fib
+  // If the number is 2 or lower
+    // return "No way, Jose"
 
-// b) Create the function that makes the test pass.
+  const greaterNum = (n) => {
+    let fib = [1,1]
+    if (n>2)
+    for (let i=2; i<n; i++) {
+      fib[i] = fib[i-1] + fib[i-2]}
+    return fib
+    if (n<2) {
+      return "No way, Jose"
+    }
+  }
+  // Test Suites: 1 passed, 1 total
+  // Tests:       1 passed, 1 total
+  // So, the test passed and works partially in a separate terminal. When I log a number less than 2 I still get an array of the same length as the number I input. If I didn't add a conditional statement to the function, then I would feel more confident. However, I tried adding curly brackets after line 51, but I still get an unexpected error and I can't move line 48 above 47 because the variable has to be defined inside the function first. Is a conditional necessary for this?
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
-
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
+describe("studyTime", () => {
+  it("takes in an object and returns an array of the values sorted from least to greatest.", () => {
+  const studyMinutesWeek1 = {
+    sunday: 90,
+    monday: 30,
+    tuesday: 20,
+    wednesday: 15,
+    thursday: 30,
+    friday: 15,
+    saturday: 60
+  }
+  // Expected output: [15, 15, 20, 30, 30, 60, 90]
+  const studyMinutesWeek2 = {
+    sunday: 100,
+    monday: 10,
+    tuesday: 45,
+    wednesday: 60,
+    thursday: 20,
+    friday: 15,
+    saturday: 65
+  }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
+  expect(studyTime(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+  expect(studyTime(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+})
+}
+)
+// ReferenceError: studyTime is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
 
-// b) Create the function that makes the test pass.
+// Psuedocode:
+// Create a function called studyTime
+// Has one parameter:an object
+// Declare a variable orderedTime that is set at the Object.values to initially list out all the elements in the input array.
+  // Use the .sort() method to sort the array from smallest to largest 
+      // The .sort() method will automatically sort the numbers in the array from smallest to largest, with one caveat. .sort recognizes 100 as larger than 20 becuase it compares first character to first character (1 > 2). Thus, to fix this, we have to use the compare function. The compare function compares two VALUES at a time (a, b). If we were to compare 50 and 100, the sort method would subtract a from b 50 -100  and would return a negative number, which sort() would then recognize 50 as the lower number.
+
+const studyTime = (object) => {
+let orderedTime = Object.values(object).sort(function(a, b) {return a - b})
+return orderedTime}
+
+// }Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+
+
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("newArray", () => {
+  it ("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+    const accountTransactions1 = [100, -17, -23, -9]
+    // Expected output: [100, 83, 60, 51]
+    const accountTransactions2 = [250, -89, 100, -96]
+    // Expected output: [250, 161, 261, 165]
+    const accountTransactions3 = []
+    // Expected output: []
+    expect(newArray(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(newArray(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(newArray(accountTransactions3)).toEqual([])
+  }
+  )
+}
+)
+//ReferenceError: newArray is not defined
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 2 passed, 3 total
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+// Pseudocode:
+// Create a function: newArray 
+// Has one parameter: an array 
+// Since we want to update every element in the array and return an array of the same length, we will use the .map() method
+// We are iterating through every element, as indicated above, and trying to find the accumulating sum of 2 values (first value + second value, that value plus the third value, and so on)
+  // Use the cumulativeSum function: (sum => value => sum += value) 
+    // It is especially important to set the sum to zero at the end of the function so that every time sum is called on, it will update to be the become the previous value. 
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
+  const newArray = (array) => {
+    return array.map((sum => value => sum += value)(0))
+  }
+  // var myarray = [5, 10, 3, 2];
+  // var new_array = [];
+  // myarray.reduce(function(a,b,i) { return new_array[i] = a+b; },0);
 
-const accountTransactions3 = []
-// Expected output: []
-
-// b) Create the function that makes the test pass.
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,54 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is a successful hybrid of running JavaScript and HTML flavored langauges together in React. This allows for programmers to write logical and format all on one page instead of having different tabs for each respective language. One syntax difference between HTML and JSX is their naming conventions; HTML uses attribute, while JSX uses className. 
 
-Researched answer:
+Researched answer: JSX is used in React to display what the UI (user interface) will look like. It is an extension of JavaScript and allows programmers the ability to display logic and markup languages on a single page for successful deployment. Although JSX is not required in React, many programmers find it beneficial because of the reported errors and warning messages. There are many differences between JSX and HTML. HTML has optional forward slashes in its closing tag, whereas the forward slash is required to close a tag in JSX. Additionally, event listeners in JSX are written in camelCase, like onClick, whereas they are written in all lowercase letter, like onclick, in HTML. 
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager for jest. Yarn manages all files and snippets of code required to run the jest test properly. Yarn is responsible for deploying a jest test to allow a programmer to see where their tests fail or pass and how to remediate. The files that are modified in a React application when you run the command yarn in your terminal are pacakge.json and yarn.lock. 
 
-Researched answer:
+Researched answer: Yarn was created by Facebook in 2016 becuase of the significant amount of dependencies and the requirement for all of them to work together in order to properly run properly. Yarn is a package manager that manages and installs the dependency files, and provides the proper terminal commands to run tests. To install dependencies, the command $yarn add jest should be ran. This command creates a directory, called node_modules and creates the files package.json and yarn.lock, which are also the files modified in a React app when the yarn command is ran in the terminal. 
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: An anonymous function is a function without a name that can be used as an argument for another function. 
 
-Researched answer:
+Researched answer: An anonymous function is a function without a name and is self-invoking. An anonymous function is best identified with the arrow syntax let add = () => {}. Similar to a traditional function expression, an anonymous function must be declared before it is invoked; it is not hoisted in JavaScript. Anonymous functions do not have their own "this" property, as they inherently assume the scope as their unwritten "this". Lastly, anonymous functions can create a more condensed function with the same functionality as a traditional function. 
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: While they both store data, the difference between state values and props in React are state values can constantly be changed depending on user input, whereas, props, short for properties, cannot be changed as they are read-only. 
 
-Researched answer:
+Researched answer: Props pass data back and forth between components, similar to parameters in a function as they pass data to a function. However, state is contained inside the component, like a variable that has been defined in a function. Props are external, state is internal. State values can constantly be changed, props cannot becuase they are only read-only.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM is short for Document Object Model and displays the webpage for the corresponding code stored in React.
 
-Researched answer:
+Researched answer: DOM is the visual representation of the data stored in React. DOM allows programmers to visualize a webpage and modify it's structure, logic, or style. Although DOM is not a scripting language, without it JavaScript programmers would not have the ability to view an example of a webpage or an HTML document, or even their corresponding components. The DOM represents a webpage via an object sequence, and is so important because it allows a programmer to add new or modify existing objects in JavaScript to alter a webpage.
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: I believe the difference between a div and a span is div creates a container block for styling purposes and organization, whereas a span encapsulates the entire page of code. 
 
-Researched answer:
+Researched answer: While both div and span section off like parts of a webpage, they are have different functionality purposes. A div creates block-level organization like <div>
+                                    <h1>Hello World!</h1>
+                                    <p>So long, World!</p>
+                                    </div>
+
+However, a span is used for inline organization. Span adds a styling element to a specific line of code before the outside tag closes. 
+<p>First paragraph is <span id ="new text> and written here</span>for now</p>
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Object-oriented programming (OOP) manages the complexity of massive software systems. OOP organizes large software systems around objects or data instead of logic and functions. OOP controls the objects that need to be altered, rather than the functionality of them. 
 
-2. Ruby:
+2. Ruby: Ruby is an object-oriented language. Due to Ruby's sole focus on objects, its common slogan is "everything is an object". ALL data types in Ruby are objects, which means its data types are instances of classes. Mac's are pre-programmed with Ruby, so to run Ruby in a Mac, open the terminal and run the command $irb. 
 
-3. Implicit return:
+3. Implicit return: An implicit return is used in Ruby. If no return is written, the previous line's method value will be returned instead. 
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are anonymous functions sent to a method to render an outcome, if a method requires more data to alter the method action. Ruby blocks are written in between the keywords 'do' and 'end'.
 
-5. Ruby hashes:
+5. Ruby hashes: A conglomoration of unique keys and their corresponding values.
